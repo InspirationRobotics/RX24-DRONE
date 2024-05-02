0ceb1bbbeb9964d43511bf9b1cac56fbde1ec21f
Eesh Vij <eesh.vij@gmail.com>
2024-05-02 00:29:34 -0700

    Updated README

--- README.md
+++ README.md
@@ -10,3 +10,7 @@ rosdep install --from-paths src --ignore-src
 colcon build
 
 sudo cp -a install/ardupilot_msgs/local/lib/python3.10/dist-packages/. /opt/ros/humble/local/lib/python3.10/dist-packages/
+
+The above only helps with fixing linting issues. Before running in a new terminal you will also need to source the workspace everytime with:
+
+source msg_ws/install/setup.bash
--- test.py
+++ test.py
@@ -4,23 +4,24 @@ from ardupilot_msgs.msg import GlobalPosition
 import time
 import threading
 
-testPublish = Topic_Service("test", String, "publisher")
-testSub = Topic_Service("test", String, "subscriber")
+testPublish = Topic_Service("test", GlobalPosition, "publisher")
+testSub = Topic_Service("test", GlobalPosition, "subscriber")
 
 testNode = RCLPY_Handler("testNode")
 
-def test_callback(data : String):
-    print("Received: " + data.data)
+def test_callback(data : GlobalPosition):
+    print("Received: " + str(data.latitude))
 
 testNode.create_topic_publisher(testPublish)
 testNode.create_topic_subscriber(testSub, test_callback)
 
 def test_method():
     for i in range(10):
-        data = String()
-        data.data = str(i)
+        data = GlobalPosition()
+        data.latitude = float(i)
         testNode.publish_topic(testPublish, data)
         time.sleep(0.5)
+    testNode.disconnect()
 
 test_thread = threading.Thread(target=test_method, daemon=True)
 test_thread.start()
